e3db69eb5d87eea443a0ac0de8018d73e3868d0c
Mariana Marques <99043066+ma-oliveiramarques@users.noreply.github.com>
2022-06-02 21:06:35 +0100

    Add files via upload
    HTML (index), CSS (style), JavaScript (main) files upload.

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'ma-oliveiramarques',
+  youtube: 'jakelinygracielly',
+  facebook: 'mariana.marques.940098',
+  instagram: 'mar.olivmarques',
+  twitter: 'maykbrito'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
